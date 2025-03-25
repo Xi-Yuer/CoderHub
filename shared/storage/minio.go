16a6beea3782466f9d084fa1d0e4e54c1f6e5afd
Xi-Yuer <2214380963@qq.com>
2025-03-25 22:48:17 +0800

    fix(storage): 修复 Minio GetPublicURL 方法的主机地址
    - 将 GetPublicURL 方法中的主机地址从 "localhost:9000" 修改为 "minio:9000"
    - 这个修改确保了在容器化环境中正确生成公开访问 URL
    
    Signed-off-by: Xi-Yuer <2214380963@qq.com>

--- shared/storage/minio.go
+++ shared/storage/minio.go
@@ -309,5 +309,5 @@ func (m *Minio) UploadFileWithInfo(bucketName, objectName string, reader io.Read
 
 // GetPublicURL 获取永久公开访问的URL
 func (m *Minio) GetPublicURL(objectName string) string {
-	return fmt.Sprintf("http://%s/%s/%s", "localhost:9000", m.Bucket, objectName)
+	return fmt.Sprintf("http://minio:9000/%s/%s", m.Bucket, objectName)
 }
