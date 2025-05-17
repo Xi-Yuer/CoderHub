8286ccca9e71480fe31970bf5fb663df51f3f355
小鱼儿 <2214380963@qq.com>
2025-05-17 19:54:41 +0800

    feat(security): 修改 JWT 令牌的过期时间
    - 将 JWT 令牌的过期时间从 24 小时调整为 7 天
    - 此修改增加了用户认证的便利性，减少了频繁重新登录的需求
    
    Signed-off-by: 小鱼儿 <2214380963@qq.com>

--- shared/security/jwt.go
+++ shared/security/jwt.go
@@ -19,13 +19,13 @@ func GenerateAuthorization(userID int64) (string, error) {
 	claims := CustomClaims{
 		UserID: userID, // 将 userID 放入自定义字段中
 		RegisteredClaims: jwt.RegisteredClaims{
-			ID:        strconv.FormatInt(userID, 10),                      // 唯一标识
-			Issuer:    "CoderHub",                                         // 签发者
-			IssuedAt:  jwt.NewNumericDate(time.Now()),                     // 签发时间
-			Subject:   "auth",                                             // 主题
-			Audience:  jwt.ClaimStrings{"CoderHub"},                       // 接收者
-			NotBefore: jwt.NewNumericDate(time.Now()),                     // 生效时间
-			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)), // 过期时间
+			ID:        strconv.FormatInt(userID, 10),                          // 唯一标识
+			Issuer:    "CoderHub",                                             // 签发者
+			IssuedAt:  jwt.NewNumericDate(time.Now()),                         // 签发时间
+			Subject:   "auth",                                                 // 主题
+			Audience:  jwt.ClaimStrings{"CoderHub"},                           // 接收者
+			NotBefore: jwt.NewNumericDate(time.Now()),                         // 生效时间
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(7 * 24 * time.Hour)), // 过期时间
 		},
 	}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
